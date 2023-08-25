7ecc09ba571fc549dcb61de5b296f57a9a649102
kykaroo <boris_nemcev@mail.ru>
2023-08-25 21:15:52 +0500

    Добавлен абстрактный класс AreaOfEffect

---
+++ Assets/Scripts/Game/Guns/Modifiers/ProjectileCollisionEffects/AreaOfEffect.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using Game.Guns.Configs;
+using Game.Player;
+using UnityEngine;
+
+namespace Game.Guns.Modifiers.ProjectileCollisionEffects
+{
+    public abstract class AreaOfEffect : ICollisionHandler
+    {
+        public float AreaRadius;
+        public AnimationCurve DamageFallOff;
+        public int BaseDamage;
+        
+        private LayerMask hitMask;
+        private Collider[] _hitObjects = new Collider[100];
+        protected int Hits;
+
+        public AreaOfEffect(float areaRadius, AnimationCurve damageFallOff, int baseDamage)
+        {
+            AreaRadius = areaRadius;
+            DamageFallOff = damageFallOff;
+            BaseDamage = baseDamage;
+            hitMask = LayerMask.GetMask("AreaOfEffectCollider", "DestructibleObjects");
+        }
+
+        public virtual void HandleImpact(Collider impactedObject, Vector3 hitPosition, Vector3 hitNormal, GunScriptableObject gun)
+        {
+            Hits = Physics.OverlapSphereNonAlloc(hitPosition, AreaRadius, _hitObjects,
+                hitMask);
+
+            for (int i = 0; i < Hits; i++)
+            {
+                if (_hitObjects[i].transform.root.TryGetComponent(out IDamageable damageable))
+                {
+                    float distance = Vector3.Distance(hitPosition, _hitObjects[i].ClosestPoint(hitPosition));
+                    
+                    damageable.TakeDamage(Mathf.CeilToInt(BaseDamage * DamageFallOff.Evaluate(distance / AreaRadius)), default);
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/Game/Guns/Modifiers/ProjectileCollisionEffects/Explode.cs
+++ Assets/Scripts/Game/Guns/Modifiers/ProjectileCollisionEffects/Explode.cs
@@ -4,37 +4,8 @@ using UnityEngine;
 
 namespace Game.Guns.Modifiers.ProjectileCollisionEffects
 {
-    public class Explode : ICollisionHandler
+    public class Explode : AreaOfEffect
     {
-        public float ExplosionRadius;
-        public AnimationCurve DamageFallOff;
-        public int BaseDamage;
-        
-        private LayerMask hitMask;
-        private Collider[] _hitObjects = new Collider[100];
-
-        public Explode(float explosionRadius, AnimationCurve damageFallOff, int baseDamage)
-        {
-            ExplosionRadius = explosionRadius;
-            DamageFallOff = damageFallOff;
-            BaseDamage = baseDamage;
-            hitMask = LayerMask.GetMask("ExplosionCollider", "DistractibleObjects");
-        }
-
-        public void HandleImpact(Collider impactedObject, Vector3 hitPosition, Vector3 hitNormal, GunScriptableObject gun)
-        {
-            int hits = Physics.OverlapSphereNonAlloc(hitPosition, ExplosionRadius, _hitObjects,
-                hitMask);
-
-            for (int i = 0; i < hits; i++)
-            {
-                if (_hitObjects[i].transform.root.TryGetComponent(out IDamageable damageable))
-                {
-                    float distance = Vector3.Distance(hitPosition, _hitObjects[i].ClosestPoint(hitPosition));
-                    
-                    damageable.TakeDamage(Mathf.CeilToInt(BaseDamage * DamageFallOff.Evaluate(distance / ExplosionRadius)), default);
-                }
-            }
-        }
+        public Explode(float areaRadius, AnimationCurve damageFallOff, int baseDamage) : base(areaRadius, damageFallOff, baseDamage) { }
     }
 }
