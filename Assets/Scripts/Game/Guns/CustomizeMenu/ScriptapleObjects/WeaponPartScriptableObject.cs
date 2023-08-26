b324f98a0a6a211f8f94412d328211115ab03e02
kykaroo <boris_nemcev@mail.ru>
2023-08-26 23:05:02 +0500

    Добавлены UI элементы в окно кастомизации оружия

---
+++ Assets/Scripts/Game/Guns/CustomizeMenu/ScriptapleObjects/WeaponPartScriptableObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Weapon Part Type", fileName = "Customize/Weapon Part Type")]
+public class WeaponPartScriptableObject : ScriptableObject
+{
+    public enum PartType
+    {
+        Barrel,
+        Muzzle,
+        Underbarrel,
+        Stock,
+        Grip,
+        Scope,
+        Magazine
+    }
+
+    public PartType partType;
+    public Transform prefab;
+}
